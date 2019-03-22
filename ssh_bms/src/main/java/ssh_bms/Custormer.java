96a9cd318a72ead3864d141a524592597db8c5c9
HRenDone <HRenDone@163.com>
2019-03-22 10:49:09 +0800

    2019-3-22 first commit test

---
+++ ssh_bms/src/main/java/ssh_bms/Custormer.java
@@ -0,0 +1,68 @@
+package ssh_bms;
+
+public class Custormer {
+	
+	private int c_Id;
+	private String c_FirstName;
+	private String c_sex;
+	private String c_Tel;
+	private String c_Address;
+	private String c_LastName;
+	private double c_monye;
+	private String state1;
+	
+	
+	public int getC_Id() {
+		return c_Id;
+	}
+	public void setC_Id(int c_Id) {
+		this.c_Id = c_Id;
+	}
+	public String getC_FirstName() {
+		return c_FirstName;
+	}
+	public void setC_FirstName(String c_FirstName) {
+		this.c_FirstName = c_FirstName;
+	}
+	public String getC_sex() {
+		return c_sex;
+	}
+	public void setC_sex(String c_sex) {
+		this.c_sex = c_sex;
+	}
+	public String getC_Tel() {
+		return c_Tel;
+	}
+	public void setC_Tel(String c_Tel) {
+		this.c_Tel = c_Tel;
+	}
+	public String getC_Address() {
+		return c_Address;
+	}
+	public void setC_Address(String c_Address) {
+		this.c_Address = c_Address;
+	}
+	public String getC_LastName() {
+		return c_LastName;
+	}
+	public void setC_LastName(String c_LastName) {
+		this.c_LastName = c_LastName;
+	}
+	public double getC_monye() {
+		return c_monye;
+	}
+	public void setC_monye(double c_monye) {
+		this.c_monye = c_monye;
+	}
+	public String getState1() {
+		return state1;
+	}
+	public void setState1(String state1) {
+		this.state1 = state1;
+	}
+	
+	
+	
+	
+
+}
